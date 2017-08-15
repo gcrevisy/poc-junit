8a9bade60f91a330a686e4ce0edccda54e6abc5a
gcrevisy <gcrevisy@outlook.com>
2017-08-15 13:22:16 +0200

    restructuration : mise en place de parametrized tests

--- src/test/java/fr/alteca/poc/service/GenericServiceTest.java
+++ src/test/java/fr/alteca/poc/service/GenericServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.reflections.Reflections;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,26 +17,13 @@ import java.util.Set;
 @RunWith(Parameterized.class)
 public class GenericServiceTest {
 
-    private final List<TestData> testDatas;
+    private final TestData testData;
 
     @Parameterized.Parameters
     public static List<TestData> getParams() {
         List<TestData> result = new ArrayList<TestData>();
 
-
-        return result;
-    }
-
-    public GenericServiceTest(List<TestData> items) {
-        this.testDatas = items;
-    }
-
-    @Test
-    public void nullParametersTest() throws IllegalAccessException, InstantiationException {
         Reflections reflections = new Reflections("fr.alteca.poc");
-
-        String errors = "";
-
         Set<Class<? extends Service>> classes = reflections.getSubTypesOf(Service.class);
 
         for (Class<? extends Service> service : classes) {
@@ -43,47 +31,69 @@ public class GenericServiceTest {
                 continue;
             }
 
-            Service newInstance = service.newInstance();
             List<Method> methods = Arrays.asList(service.getDeclaredMethods());
-
             for (Method method : methods) {
                 if (method.getParameterCount() == 0)
                     continue;
-                try {
-                    List<Object[]> argsList = getParamterTabs(method.getParameterTypes());
-                    for (Object[] args : argsList)
-                        method.invoke(newInstance, args);
-                    method.getDeclaringClass();
-                } catch (Exception e) {
-                    if (!(e instanceof CustomException || (e.getCause() != null && e.getCause() instanceof CustomException))) {
-                        errors += " \r\n " + service.getName() + "#" + method.getName() + "#" + e.getMessage();
-                    } else {
-                        Assert.assertTrue(service.getName() + "#" + method.getName() + "#" + e.getMessage(), true);
-                    }
-                    continue;
-                }
-                errors += " \r\n " + service.getName() + "#" + method.getName() + "#pas de CustomException";
+                result.addAll(getParamterTabs(method));
             }
         }
-
-        Assert.assertEquals("", errors);
+        return result;
     }
 
-    private List<Object[]> getParamterTabs(Class<?>[] parameterTypes) throws IllegalAccessException, InstantiationException {
-        List<Object[]> result = new ArrayList<Object[]>();
+    public GenericServiceTest(TestData item) {
+        this.testData = item;
+    }
 
-        for (int i = 0; i < parameterTypes.length; i++) {
+    @Test(expected = CustomException.class)
+    public void nullParametersTest() throws Throwable {
+
+        Class<? extends Service> clazz = (Class<? extends Service>) testData.getMethodToInvoke().getDeclaringClass();
+        Service instance = null;
+        try {
+            instance = clazz.newInstance();
+        } catch (InstantiationException e) {
+            Assert.fail("Erreur lors de l'instaciation de " + clazz.getName() + " : " + e.getMessage());
+        } catch (IllegalAccessException e) {
+            Assert.fail("Erreur lors de l'instaciation de " + clazz.getName() + " : " + e.getMessage());
+        }
+
+        try {
+            testData.getMethodToInvoke().invoke(instance, testData.getParams());
+        } catch (IllegalAccessException e) {
+            Assert.fail("Erreur lors de l'appel de " + testData.getMethodToInvoke().getName() + " : " + e.getMessage());
+        } catch (InvocationTargetException e) {
+            if (e.getTargetException() instanceof CustomException) {
+                throw e.getTargetException();
+            } else {
+                Assert.fail("Erreur lors de l'appel de " + testData.getMethodToInvoke().getName() + " : " + e.getMessage());
+            }
+        }
+    }
+
+    private static List<TestData> getParamterTabs(Method method) {
+        List<TestData> result = new ArrayList<TestData>();
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        for (int i = 0; i < method.getParameterTypes().length; i++) {
             Object[] var = new Object[parameterTypes.length];
             int index = 0;
             for (Class clazz : parameterTypes) {
                 if (i == index) {
                     var[index] = null;
                 } else {
-                    var[index] = clazz.newInstance();
+                    try {
+                        var[index] = clazz.newInstance();
+                    } catch (InstantiationException e) {
+                        Assert.fail("Erreur lors de l'instaciation de " + clazz.getName() + " : " + e.getMessage());
+                        var[index] = null;
+                    } catch (IllegalAccessException e) {
+                        Assert.fail("Erreur lors de l'instaciation de " + clazz.getName() + " : " + e.getMessage());
+                        var[index] = null;
+                    }
                 }
                 index++;
             }
-            result.add(var);
+            result.add(new TestData(method, var));
         }
 
         return result;
