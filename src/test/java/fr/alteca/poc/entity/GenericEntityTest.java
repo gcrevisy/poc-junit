a8a301434b4ae954da35be6a422c5d6df0c5981a
Guillaume Crevisy <gcrevisy@outlook.com>
2019-01-14 22:19:53 +0100

    modification du TU

--- src/test/java/fr/alteca/poc/entity/GenericEntityTest.java
+++ src/test/java/fr/alteca/poc/entity/GenericEntityTest.java
@@ -1,45 +1,42 @@
 package fr.alteca.poc.entity;
 
+import java.lang.reflect.Modifier;
+import java.util.Set;
+
 import org.junit.Test;
 import org.reflections.Reflections;
 
-import java.util.Set;
-
 public class GenericEntityTest {
 
-    @Test
-    public void entitiesInstanciationTest() {
+	@Test
+	public void entitiesInstanciationTest() {
 
-        Reflections reflections = new Reflections("fr.alteca.poc");
+		Reflections reflections = new Reflections("fr.alteca.poc");
 
-        String errors = "";
+		String errors = "";
 
-        Set<Class<? extends Entity>> entities = reflections.getSubTypesOf(Entity.class);
+		Set<Class<? extends Entity>> entities = reflections.getSubTypesOf(Entity.class);
 
-        for (Class<? extends Entity> entity : entities) {
-            Entity instance = null;
-            try {
-                instance = entity.newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-/*
-            List<Field> fields = Arrays.asList(entity.getDeclaredFields());
-            if (instance != null && fields.size() > 0) {
-                for (Field field : fields) {
-                    try {
-                        Object value = field.get(instance);
-                        if (value == null) {
-                            Assert.fail(entity.getName() + "#" + field.getName());
-                        }
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }*/
-        }
+		for (Class<? extends Entity> entity : entities) {
+			if (Modifier.isAbstract(entity.getModifiers())) {
+				continue;
+			}
+			Entity instance = null;
+			try {
+				instance = entity.newInstance();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			/*
+			 * List<Field> fields = Arrays.asList(entity.getDeclaredFields()); if (instance
+			 * != null && fields.size() > 0) { for (Field field : fields) { try { Object
+			 * value = field.get(instance); if (value == null) {
+			 * Assert.fail(entity.getName() + "#" + field.getName()); } } catch
+			 * (IllegalAccessException e) { e.printStackTrace(); } } }
+			 */
+		}
 
-    }
+	}
 }
